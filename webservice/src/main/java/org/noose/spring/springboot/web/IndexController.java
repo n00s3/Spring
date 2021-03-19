d23b28f854ef6d19696fcd0d3c89eab87c49dd2b
noose <hd15807@gmail.com>
2021-03-19 19:22:27 +0900

    oauth2.0(google, naver) 추가

--- webservice/src/main/java/org/noose/spring/springboot/web/IndexController.java
+++ webservice/src/main/java/org/noose/spring/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package org.noose.spring.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.noose.spring.springboot.config.auth.LoginUser;
+import org.noose.spring.springboot.config.auth.dto.SessionUser;
 import org.noose.spring.springboot.service.posts.PostsService;
 import org.noose.spring.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    private  final PostsService postsService;
+    private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
