9170d4fc52f194f1d384a0a8b1f7e324f29bca26
nooose <hd15807@gmail.com>
2022-04-18 16:29:49 +0900

    프록시 패턴 예제 생성

---
+++ proxy/src/main/java/hello/proxy/app/v1/OrderControllerImplV1.java
@@ -0,0 +1,19 @@
+package hello.proxy.app.v1;
+
+public class OrderControllerImplV1 implements OrderControllerV1 {
+    private final OrderServiceV1 orderServiceV1;
+
+    public OrderControllerImplV1(OrderServiceV1 orderServiceV1) {
+        this.orderServiceV1 = orderServiceV1;
+    }
+
+    @Override
+    public String request(String itemId) {
+        return "ok";
+    }
+
+    @Override
+    public String noLog() {
+        return "ok";
+    }
+}
